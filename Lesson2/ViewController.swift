1f83d03d989af760b9f5dfa3c9c2701ef70de0c8
Ivan Ch <vano3d@gmail.com>
2021-05-22 16:53:09 +0300

    hex code added

--- Lesson2/ViewController.swift
+++ Lesson2/ViewController.swift
@@ -7,6 +7,37 @@
 
 import UIKit
 
+extension UIColor {
+    
+    convenience init(hex: String, alpha: CGFloat = 1.0) {
+        var hexFormatted: String = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+
+        if hexFormatted.hasPrefix("#") {
+            hexFormatted = String(hexFormatted.dropFirst())
+        }
+
+        assert(hexFormatted.count == 6, "Invalid hex code used.")
+
+        var rgbValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbValue)
+
+        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+                  alpha: alpha)
+    }
+    
+    func toHexString() -> String {
+        var r:CGFloat = 0
+        var g:CGFloat = 0
+        var b:CGFloat = 0
+        var a:CGFloat = 0
+        getRed(&r, green: &g, blue: &b, alpha: &a)
+        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+        return String(format:"#%06x", rgb)
+    }
+}
+
 class ViewController: UIViewController {
 
     
@@ -20,6 +51,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var valueRed: UILabel!
     @IBOutlet weak var valueGreen: UILabel!
     @IBOutlet weak var valueBlue: UILabel!
+    
+    @IBOutlet weak var hexColor: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,7 +68,12 @@ class ViewController: UIViewController {
         valueRed.text = String(format: "%.1f", sliderRed.value)
         valueGreen.text = String(format: "%.1f", sliderGreen.value)
         valueBlue.text = String(format: "%.1f", sliderBlue.value)
-        viewColor.backgroundColor = UIColor(red: CGFloat(sliderRed.value), green: CGFloat(sliderGreen.value), blue: CGFloat(sliderBlue.value), alpha: CGFloat(sliderOpacity.value))
+        
+        let bgColor = UIColor(red: CGFloat(sliderRed.value), green: CGFloat(sliderGreen.value), blue: CGFloat(sliderBlue.value), alpha: CGFloat(sliderOpacity.value))
+        
+        viewColor.backgroundColor = bgColor
+        
+        hexColor.text = bgColor.toHexString()
         
     }
     
